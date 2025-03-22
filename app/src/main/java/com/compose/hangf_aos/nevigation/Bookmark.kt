c3db15c31d68e7798ccaf12a99290d180b895568
MunKiJeon <jmk070994@gmail.com>
2025-03-23 00:09:34 +0900

    add 인터넷 권한 추가 예약 화면 추가 확정 화면 네비게이션 사용자 정보 화면 홈 화면 더미 사진 파일 추가

---
+++ app/src/main/java/com/compose/hangf_aos/nevigation/Bookmark.kt
@@ -0,0 +1,13 @@
+package com.compose.hangf_aos.nevigation
+
+enum class Bookmark {
+    Splash,
+    MainHome,
+    CustomerInfo,
+    CustomerReservation,
+    CustomerConfirmed,
+    CustomerLookup,
+    StoreOwnerInfo,
+    StoreOwnerReservation,
+    StoreOwnerMenu,
+}
